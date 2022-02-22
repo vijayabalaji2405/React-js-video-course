8879f29ccbedb36eae5d1986b1a76dd108bc7069
vijayabalaji <vijaya.balaji@codem.com>
2022-02-22 14:21:38 +0530

    completed

---
+++ Project-4/src/components/UI/Input/Input.js
@@ -0,0 +1,38 @@
+import React, { useRef, useImperativeHandle } from "react";
+
+import classes from "./Input.module.css";
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    console.log(ref);
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus1: activate,
+    };
+  });
+
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
